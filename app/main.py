591b3f4bc67fe33f0bdb13b36cc081fe96912962
seancze <60085582+seancze@users.noreply.github.com>
2024-08-16 00:43:05 +0800

    fix: cors error

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 import uvicorn
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.conn import lifespan
 from app.api.routes import router
 
 app = FastAPI(lifespan=lifespan)
+
+# Configure CORS
+origins = [
+    "http://localhost",
+    "https://reddit-llm-frontend.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 # heroku may set the port dynamically
